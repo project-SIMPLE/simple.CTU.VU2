edaab914498319cbd530f4ec2c3809ce04478996
K1roww <dinhpro2005@gmail.com>
2025-09-03 16:57:43 +0700

    #1 Update  UI Dinh
     Update  UI Dinh

---
+++ Assets/_SCRIPTS/Dinh_23034/PlantArea.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class PlantArea : MonoBehaviour
+{
+    [Header("Setup")]
+    public Transform plantPoint;   // Điểm trồng cây
+    public int panelIndex;         // panel UI nào (0-3)
+    private GameObject currentPlant;
+
+    private bool playerInside = false;
+    private bool isUIOpen = false;
+
+    // static để biết plot nào đang active
+    public static PlantArea currentActivePlot;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+            currentActivePlot = this;   // ghi nhớ plot hiện tại
+            Debug.Log("Player vào plot: " + gameObject.name + " -> Panel " + panelIndex);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+            if (currentActivePlot == this) currentActivePlot = null;
+            PlantUIManager.Instance.HideUI();
+            Debug.Log("Player rời plot: " + gameObject.name);
+        }
+    }
+
+    void Update()
+    {
+        if (playerInside)
+        {
+            // Bấm bàn phím test
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                if (!isUIOpen)
+                {
+                    PlantUIManager.Instance.ShowUI(panelIndex);
+                    Debug.Log("Show UI cho plot: " + gameObject.name + " -> Panel " + panelIndex);
+                }
+                else
+                {
+                    PlantUIManager.Instance.HideUI();
+                    Debug.Log("Hide UI cho plot: " + gameObject.name);
+                }
+
+                isUIOpen = !isUIOpen; // đảo trạng thái
+            }
+
+            // VR controller
+            InputDevice rightHand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+            if (rightHand.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed) && buttonPressed)
+            {
+                PlantUIManager.Instance.ShowUI(panelIndex);
+                Debug.Log("Show UI cho plot: " + gameObject.name + " -> Panel " + panelIndex);
+            }
+        }
+    }
+
+    // Gọi khi chọn cây
+    public void Plant(GameObject plantPrefab)
+    {
+        if (currentPlant != null) Destroy(currentPlant);
+        currentPlant = Instantiate(plantPrefab, plantPoint.position, plantPoint.rotation, plantPoint);
+        Debug.Log("Đã trồng " + plantPrefab.name + " vào plot " + gameObject.name);
+    }
+}
---
+++ Assets/_SCRIPTS/Dinh_23034/PlantUIManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlantUIManager : MonoBehaviour
+{
+    public static PlantUIManager Instance; // Singleton để gọi từ PlantArea
+    public GameObject[] uiPanels; // chứa 4 UI panel
+    private int currentUI = -1;
+
+    void Awake()
+    {
+        Instance = this;
+        HideAllUI();
+    }
+
+    public void ShowUI(int index)
+    {
+        // Nếu UI này đang mở -> bấm lại sẽ tắt
+        if (currentUI == index)
+        {
+            HideUI();
+            return;
+        }
+
+        HideAllUI(); // ẩn hết trước
+        if (index >= 0 && index < uiPanels.Length)
+        {
+            GameObject panel = uiPanels[index];
+            panel.SetActive(true);
+            currentUI = index;
+
+            // --- Đặt UI trước mặt player ---
+            Transform cam = Camera.main.transform;
+            float distance = 1f; // khoảng cách trước mặt
+            Vector3 targetPos = cam.position + cam.forward * distance;
+            targetPos.y = cam.position.y; // ngang tầm mắt
+
+            panel.transform.position = targetPos;
+
+            // quay panel nhìn về phía player
+            panel.transform.LookAt(cam);
+            panel.transform.Rotate(0, 180f, 0); // lật lại vì canvas mặc định ngược
+        }
+    }
+
+    public void HideUI()
+    {
+        if (currentUI >= 0 && currentUI < uiPanels.Length)
+        {
+            uiPanels[currentUI].SetActive(false);
+            currentUI = -1;
+        }
+    }
+
+    private void HideAllUI()
+    {
+        foreach (var panel in uiPanels)
+        {
+            panel.SetActive(false);
+        }
+        currentUI = -1;
+    }
+}
