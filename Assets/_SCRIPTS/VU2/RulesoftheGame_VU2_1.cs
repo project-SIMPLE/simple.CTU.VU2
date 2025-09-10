6a209e90c4afe93f62856ebc2a448fadffa4c943
Nguyen Hong Son <son.nguyenhong1990@gmail.com>
2025-09-10 17:17:37 +0700

    update
    vat nuoi

--- Assets/_SCRIPTS/VU2/RulesoftheGame_VU2_1.cs
+++ Assets/_SCRIPTS/VU2/RulesoftheGame_VU2_1.cs
@@ -10,37 +10,62 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 	public GameObject Weather_Rain;
 	public Text clockText;
 	public float timeRemaining = 0;
-	
+	public bool playGame = false;
+	public GameObject Rain_image;
+	public GameObject Sun_image;
+	public Material Skybox_Rain;
+	public Material Skybox_Sun;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playGame = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (true)
+        if (playGame == true)
 		{
 			timeRemaining += Time.deltaTime;
 			DisplayTime(timeRemaining);
 
-			if (timeRemaining > 150)
+			if (timeRemaining <=90)
 			{
-				Debug.Log("Mùa Mưa đã đến!");
-				//Weather_Rain.SetActive(true);
+				//Debug.Log("Mùa Mưa bắt đầu!");
+				Weather_Rain.SetActive(true);
+				Rain_image.SetActive(true);
+				Sun_image.SetActive(false);
+				RenderSettings.skybox = Skybox_Rain;
+        		DynamicGI.UpdateEnvironment();
 			}
-			if (timeRemaining > 275)
+			else if (timeRemaining > 90 && timeRemaining <=160)
 			{
-				Debug.Log("Thoại NPC - Hướng dẫn di chuyển tầng giữa");
-				Debug.Log("Cho phép di chuyển tầng giữa");
-
+				//Debug.Log("Mùa Khô bắt đầu!");
+				Weather_Rain.SetActive(false);
+				Rain_image.SetActive(false);
+				Sun_image.SetActive(true);
+				RenderSettings.skybox = Skybox_Sun;
+        		DynamicGI.UpdateEnvironment();
 			}
-			if (timeRemaining > 105)
+			else if (timeRemaining > 160 && timeRemaining<=180)
 			{
-				Debug.Log("Thoại NPC - Hướng dẫn di chuyển tầng đáy");
-				Debug.Log("Cho phép di chuyển tầng đáy");
-				
+				//Debug.Log("Mùa Mưa Trở Lại!");
+				Weather_Rain.SetActive(true);
+				Rain_image.SetActive(true);
+				Sun_image.SetActive(false);
+				RenderSettings.skybox = Skybox_Rain;
+        		DynamicGI.UpdateEnvironment();
+			}
+			else
+			{
+				//Debug.Log("Màn chơi kết thúc.");
+				playGame = false;
+				Weather_Rain.SetActive(false);
+				Rain_image.SetActive(false);
+				Sun_image.SetActive(true);
+				RenderSettings.skybox = Skybox_Sun;
+        		DynamicGI.UpdateEnvironment();
 			}
 		}
     }
