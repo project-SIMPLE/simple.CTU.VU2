9be738cc27808ffadda01a6eec07df41a3fe5e51
Nguyen Hong Son <son.nguyenhong1990@gmail.com>
2025-09-21 12:49:45 +0700

    Setup Demo
    Setup Demo

--- Assets/_SCRIPTS/VU2/RulesoftheGame_VU2_1.cs
+++ Assets/_SCRIPTS/VU2/RulesoftheGame_VU2_1.cs
@@ -16,17 +16,47 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 	public Material Skybox_Rain;
 	public Material Skybox_Sun;
 
+	// Music
+	public AudioClip rainMusic;
+	public AudioClip normalMusic;
+    private AudioSource audioSource;
+
+	// saltwater intrusion
+	//public Saltwater_Intrusion saltwaterIntrusionObject;
+	public GameObject target;       // Object cần di chuyển
+    public Vector3 pointA;          // Vị trí bắt đầu
+    public Vector3 pointB;          // Vị trí kết thúc
+    public float moveTime = 3f;     // Thời gian di chuyển (giây)
+
+    private float timer;
+    private bool moving;
+
     // Start is called before the first frame update
     void Start()
     {
         playGame = true;
+
+		audioSource = GetComponent<AudioSource>();
+        //PlayMusic(normalMusic);
+
+		target.transform.position = pointA;
+		timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+		// Nhấn Space để bắt đầu di chuyển
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+
+        
+
         if (playGame == true)
 		{
+			//saltwaterIntrusionObject.StartMove();
 			timeRemaining += Time.deltaTime;
 			DisplayTime(timeRemaining);
 
@@ -38,6 +68,10 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 				Sun_image.SetActive(false);
 				RenderSettings.skybox = Skybox_Rain;
         		DynamicGI.UpdateEnvironment();
+				PlayMusic(rainMusic);
+				//saltwaterIntrusionObject.StartMove();
+				
+
 			}
 			else if (timeRemaining > 90 && timeRemaining <=160)
 			{
@@ -47,6 +81,10 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 				Sun_image.SetActive(true);
 				RenderSettings.skybox = Skybox_Sun;
         		DynamicGI.UpdateEnvironment();
+				PlayMusic(normalMusic);
+				moving = true;
+				
+
 			}
 			else if (timeRemaining > 160 && timeRemaining<=180)
 			{
@@ -56,7 +94,9 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 				Sun_image.SetActive(false);
 				RenderSettings.skybox = Skybox_Rain;
         		DynamicGI.UpdateEnvironment();
+				target.transform.position = pointA;
 			}
+
 			else
 			{
 				//Debug.Log("Màn chơi kết thúc.");
@@ -66,6 +106,18 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 				Sun_image.SetActive(true);
 				RenderSettings.skybox = Skybox_Sun;
         		DynamicGI.UpdateEnvironment();
+				PlayMusic(rainMusic);
+			}
+
+			if (moving)
+			{
+				timer += Time.deltaTime;
+				float t = timer / moveTime;
+
+				target.transform.position = Vector3.Lerp(pointA, pointB, t);
+
+				if (t >= 1f)
+					moving = false;
 			}
 		}
     }
@@ -77,4 +129,15 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 		float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 		clockText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 	}
+
+	void PlayMusic(AudioClip clip)
+    {
+        if (clip == null) return;
+        if (audioSource.clip == clip) return; // nếu đang phát rồi thì bỏ qua
+
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+	
 }
