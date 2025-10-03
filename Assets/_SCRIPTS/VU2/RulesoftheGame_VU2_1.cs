051ebacbde7efa12f7ddfe2e06c115d630942e48
Nguyen Hong Son <son.nguyenhong1990@gmail.com>
2025-10-03 11:34:49 +0700

    Update Start Menu

--- Assets/_SCRIPTS/VU2/RulesoftheGame_VU2_1.cs
+++ Assets/_SCRIPTS/VU2/RulesoftheGame_VU2_1.cs
@@ -13,115 +13,155 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 	public bool playGame = false;
 	public GameObject Rain_image;
 	public GameObject Sun_image;
+
+	public GameObject StartMenu;
+	public GameObject ResultMenu;
+
 	public Material Skybox_Rain;
 	public Material Skybox_Sun;
+
+
 
 	// Music
 	public AudioClip rainMusic;
 	public AudioClip normalMusic;
-    private AudioSource audioSource;
+	private AudioSource audioSource;
 
 	// saltwater intrusion
 	//public Saltwater_Intrusion saltwaterIntrusionObject;
 	public GameObject target;       // Object cần di chuyển
-    public Vector3 pointA;          // Vị trí bắt đầu
-    public Vector3 pointB;          // Vị trí kết thúc
-    public float moveTime = 3f;     // Thời gian di chuyển (giây)
+	public Vector3 pointA;          // Vị trí bắt đầu
+	public Vector3 pointB;          // Vị trí kết thúc
+	public float moveTime = 3f;     // Thời gian di chuyển (giây)
 
-    private float timer;
-    private bool moving;
+	private float timer;
+	private bool moving;
+	private bool rainning;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        playGame = true;
-
+	// Start is called before the first frame update
+	public void Start()
+	{
+		playGame = false;
+		ResultMenu.SetActive(false);
+		StartMenu.SetActive(true);
+		Weather_Rain.SetActive(false);
+		Rain_image.SetActive(false);
+		Sun_image.SetActive(true);
 		audioSource = GetComponent<AudioSource>();
-        //PlayMusic(normalMusic);
+		PlayMusic(normalMusic);
 
 		target.transform.position = pointA;
 		timer = 0f;
-    }
+		rainning = false;
+		moving = false;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		// Nhấn Space để bắt đầu di chuyển
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            
-        }
+	// Update is called once per frame
+	public void Update()
+	{
 
-        
-
-        if (playGame == true)
+		if (playGame == true)
 		{
 			//saltwaterIntrusionObject.StartMove();
 			timeRemaining += Time.deltaTime;
 			DisplayTime(timeRemaining);
 
-			if (timeRemaining <=90)
+			if (timeRemaining <= 90)
 			{
 				//Debug.Log("Mùa Mưa bắt đầu!");
+				rainning = true;
 				Weather_Rain.SetActive(true);
 				Rain_image.SetActive(true);
 				Sun_image.SetActive(false);
 				RenderSettings.skybox = Skybox_Rain;
-        		DynamicGI.UpdateEnvironment();
+				DynamicGI.UpdateEnvironment();
 				PlayMusic(rainMusic);
 				//saltwaterIntrusionObject.StartMove();
-				
+
 
 			}
-			else if (timeRemaining > 90 && timeRemaining <=160)
+			else if (timeRemaining > 90 && timeRemaining <= 160)
 			{
 				//Debug.Log("Mùa Khô bắt đầu!");
+				rainning = false;
 				Weather_Rain.SetActive(false);
 				Rain_image.SetActive(false);
 				Sun_image.SetActive(true);
 				RenderSettings.skybox = Skybox_Sun;
-        		DynamicGI.UpdateEnvironment();
+				DynamicGI.UpdateEnvironment();
 				PlayMusic(normalMusic);
-				moving = true;
-				
+				moving = false;
+				//saltwaterIntrusionObject.StartMove();
+				//target.transform.position = pointA;
+
+				//Debug.Log("Di chuyển nước .");
+
+				timer += Time.deltaTime;
+				float t = timer / moveTime;
+
+				target.transform.position = Vector3.Lerp(pointA, pointB, t);
+
+
 
 			}
-			else if (timeRemaining > 160 && timeRemaining<=180)
+			else if (timeRemaining > 160 && timeRemaining <= 180)
 			{
 				//Debug.Log("Mùa Mưa Trở Lại!");
+				rainning = true;
+				moving = true;
 				Weather_Rain.SetActive(true);
 				Rain_image.SetActive(true);
 				Sun_image.SetActive(false);
 				RenderSettings.skybox = Skybox_Rain;
-        		DynamicGI.UpdateEnvironment();
-				target.transform.position = pointA;
+				DynamicGI.UpdateEnvironment();
+				target.transform.position = pointB;
+				PlayMusic(rainMusic);
+
+				timer += Time.deltaTime;
+				float t = timer / moveTime;
+
+				target.transform.position = Vector3.Lerp(pointB, pointA, t);
 			}
 
 			else
 			{
 				//Debug.Log("Màn chơi kết thúc.");
 				playGame = false;
+				rainning = false;
 				Weather_Rain.SetActive(false);
 				Rain_image.SetActive(false);
 				Sun_image.SetActive(true);
 				RenderSettings.skybox = Skybox_Sun;
-        		DynamicGI.UpdateEnvironment();
-				PlayMusic(rainMusic);
+				DynamicGI.UpdateEnvironment();
+				PlayMusic(normalMusic);
+				ResultMenu.SetActive(true);
+
 			}
 
-			if (moving)
-			{
-				timer += Time.deltaTime;
-				float t = timer / moveTime;
+			// if (moving== true && rainning == false)
+			// {
+			// 	Debug.Log("Di chuyển nước .");
+			// 	timer += Time.deltaTime;
+			// 	float t = timer / moveTime;
 
-				target.transform.position = Vector3.Lerp(pointA, pointB, t);
+			// 	target.transform.position = Vector3.Lerp(pointA, pointB, t);
 
-				if (t >= 1f)
-					moving = false;
-			}
+			// 	if (t >= 1f)
+			// 		moving = false;
+			// }
+			// if (moving== true && rainning == true)
+			// {
+			// 	timer += Time.deltaTime;
+			// 	float t = timer / moveTime;
+
+			// 	target.transform.position = Vector3.Lerp(pointB, pointA, t);
+
+			// 	if (t >= 1f)
+			// 		moving = false;
+			// }
 		}
-    }
-	
+	}
+
 	void DisplayTime(float timeToDisplay)
 	{
 		// timeToDisplay += 1;
@@ -131,13 +171,33 @@ public class RulesoftheGame_VU2_1 : MonoBehaviour
 	}
 
 	void PlayMusic(AudioClip clip)
-    {
-        if (clip == null) return;
-        if (audioSource.clip == clip) return; // nếu đang phát rồi thì bỏ qua
+	{
+		if (clip == null) return;
+		if (audioSource.clip == clip) return; // nếu đang phát rồi thì bỏ qua
 
-        audioSource.clip = clip;
-        audioSource.Play();
-    }
+		audioSource.clip = clip;
+		audioSource.Play();
+	}
 
+	public void StartGame()
+	{
+		Debug.Log("Play Game.");
+		playGame = true;
+		StartMenu.SetActive(false);
+		timeRemaining = 0;
+	}
+
+	public void RestartGame()
+	{
+		Debug.Log("Restart Game.");
+		playGame = true;
+		ResultMenu.SetActive(false);
+		StartMenu.SetActive(true);
+		timeRemaining = 0;
+		Weather_Rain.SetActive(false);
+		Rain_image.SetActive(false);
+		Sun_image.SetActive(true);
+	}
 	
+
 }
